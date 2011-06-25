2858502bc2e0ad0292f4a0f874b9b30b7ef23842
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2011-06-24 23:27:10 -0600

    Version numbers!

--- src/cmd/fetchd/fetchd.go
+++ src/cmd/fetchd/fetchd.go
@@ -16,16 +16,27 @@ func printResults(ids chan string) {
 }
 
 var configPath string
+var showVersion bool
 
 func init() {
     flag.StringVar(&configPath, "config", "", "Path to the config file")
+    flag.BoolVar(&showVersion, "version", false, "Show the version")
 }
 
 func main() {
     flag.Parse()
+    versionString := fmt.Sprintf("fetch v%s © Daniel Huckstep", fetch.Version)
+
+    if showVersion {
+        fmt.Println(versionString)
+        return
+    }
+
     if configPath == "" {
         panic("Must specify config file")
     }
+
+    fmt.Println("Starting", versionString)
 
     c := config.NewFromFile(configPath)
     e := fetch.Build("redis")
